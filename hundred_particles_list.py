d639fb1f60e021092bb29cd1f560eab84894a370
emilyng <32720464+emilyng@users.noreply.github.com>
2018-04-01 21:08:11 -0500

    Add files via upload

---
+++ hundred_particles_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 19:04:29 2018
+
+@author: emilyng
+"""
+
+import h5py
+
+from particle import Particle
+
+'''Read into hdf5 file with initial conidtions'''
+'''file names 'gaussian.h5' located in working directory'''
+
+hf = h5py.File("gaussian.h5", 'r')
+
+'''retrieve particles positions, velocities, and masses from hdf5 file'''
+positions = hf['particle_positions'][:100]
+velocities = hf['particle_velocities'][:100]
+masses = hf['particle_masses'][:100]
+
+'''Create list of particle objects from initial conditions'''
+particles = []
+for i in range(len(positions)):
+    particles.append(Particle(positions[i][0], positions[i][1], positions[i][2],
+                              velocities[i][0], velocities[i][1], velocities[i][2],
+                              masses[i]))
+
+
+ 
